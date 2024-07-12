f53dda8bbd8ffd610289d0259bd207b235fce851
FHoughton <fdphoughton@protonmail.com>
2024-07-12 02:13:12 +0100

    Init working files

---
+++ main.lua
@@ -0,0 +1,17 @@
+if arg[2] == "debug" then
+    require("lldebugger").start()
+end
+
+local love_errorhandler = love.errorhandler
+
+function love.errorhandler(msg)
+    if lldebugger then
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
+    end
+end
+
+function love.draw()
+    love.graphics.print("Hello World!", 100, 100)
+end
