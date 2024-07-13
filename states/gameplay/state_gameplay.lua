03bf04aee6cfccd83e71bff28734d1428f344151
FHoughton <fdphoughton@protonmail.com>
2024-07-13 04:53:53 +0100

    Camera setup

--- states/gameplay/state_gameplay.lua
+++ states/gameplay/state_gameplay.lua
@@ -1,4 +1,5 @@
 GameObject = require "states.gameplay.game_object"
+Camera = require "lib.hump.camera"
 
 local state_gameplay = {}
 
@@ -7,18 +8,26 @@ local objects = {}
 function state_gameplay:enter()
     love.graphics.setBackgroundColor(0, 1, 0)
     table.insert(objects,GameObject(100, 100))
+    camera = Camera(0, 0)
 end
 
 function state_gameplay:update(dt)
     for i, obj in ipairs(objects) do
         obj:update(dt)
+
+        local middle = obj:getMiddle()
+        camera:lookAt(middle.x, middle.y)
     end
 end
 
 function state_gameplay:draw()
+    camera:attach()
+    love.graphics.rectangle("fill", 100, 100, 300, 300)
+
     for i, obj in ipairs(objects) do
         obj:draw()
     end
+    camera:detach()
 end
 
 return state_gameplay
