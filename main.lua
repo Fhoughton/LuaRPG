69cf9bc689ba9a3cd89e1427219891a806660206
FHoughton <fdphoughton@protonmail.com>
2024-07-13 06:46:06 +0100

    Add Balatro style scaling :)

--- main.lua
+++ main.lua
@@ -1,8 +1,33 @@
 require "debug"
 
+rs = require "lib.resolution_solution"
+rs.conf({game_width = 640, game_height = 360, scale_mode = rs.ASPECT_MODE})
+rs.setMode(rs.game_width, rs.game_height, {resizable = true})
+
+rs.resize_callback = function()
+    if rs.scale_mode == rs.NO_SCALING_MODE then
+      camera_canvas = love.graphics.newCanvas(love.graphics.getWidth(), love.graphics.getHeight())
+    else
+      camera_canvas = love.graphics.newCanvas(rs.game_width, rs.game_height)
+    end
+end
+
+camera_canvas = love.graphics.newCanvas(rs.game_width, rs.game_height)
+
+love.resize = function(w, h)
+    rs.resize(w, h)
+  end
+
 local state_gameplay = require "states.gameplay.state_gameplay"
 
 Gamestate = require "lib.hump.gamestate"
+
+function love.keypressed(key, scancode, isrepeat)
+	if key == "f11" then
+		fullscreen = not fullscreen
+		love.window.setFullscreen(fullscreen, "exclusive")
+	end
+end
 
 function love.load()
     love.graphics.setBackgroundColor(0, 0.4, 1)
--- states/gameplay/state_gameplay.lua
+++ states/gameplay/state_gameplay.lua
@@ -2,6 +2,8 @@ GameObject = require "states.gameplay.game_object"
 Player = require "states.gameplay.player"
 Camera = require "lib.hump.camera"
 
+local game_canvas = love.graphics.newCanvas(rs.get_game_size())
+
 local state_gameplay = {}
 
 local objects = {}
@@ -22,13 +24,31 @@ function state_gameplay:update(dt)
 end
 
 function state_gameplay:draw()
-    camera:attach()
+    if rs.scale_mode == rs.NO_SCALING_MODE then
+        camera:attach(0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+      else
+        camera:attach(0, 0, rs.game_width, rs.game_height)
+    end
+
+    love.graphics.setCanvas(camera_canvas)
+    love.graphics.clear(1, 1, 1, 0)
+
     love.graphics.rectangle("fill", 100, 100, 300, 300)
 
     for i, obj in ipairs(objects) do
         obj:draw()
     end
+    love.graphics.setCanvas()
     camera:detach()
+    
+
+    rs.push()
+        -- Draw camera's canvas.
+        love.graphics.draw(camera_canvas)
+    rs.pop()
+
+    -- Draw rectangle to see game zone.
+    love.graphics.rectangle("line", rs.get_game_zone())
 end
 
 return state_gameplay
