5da0cc59808d38f8d858e978addf8dc5c28ba260
FHoughton <fdphoughton@protonmail.com>
2024-07-17 08:31:26 +0100

    Fix map scrolling

---
+++ tiled/outdoor_tileset.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="outdoor_tileset" tilewidth="32" tileheight="32" tilecount="180" columns="20">
+ <image source="../resources/tilesets/outdoor_tileset.png" width="640" height="288"/>
+ <tile id="48">
+  <properties>
+   <property name="isSolid" type="bool" value="true"/>
+   <property name="isWater" type="bool" value="true"/>
+   <property name="speedMultiplier" type="float" value="0.5"/>
+  </properties>
+ </tile>
+</tileset>
