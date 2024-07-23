ac1d713fbefddcdada2b5040d69f6a6dd4155385
FHoughton <fdphoughton@protonmail.com>
2024-07-23 23:33:48 +0100

    Perfect collision with debug info

--- states/gameplay/player.lua
+++ states/gameplay/player.lua
@@ -1,8 +1,11 @@
 local peachy = require("lib.peachy")
+local debug = require "debug"
 
 local GameObject = require "states.gameplay.game_object"
 
 local Player = GameObject:extend()
+
+inspect = require "lib.inspect"
 
 function Player:new(x, y, camera)
     self.image = peachy.new("resources/man.json", love.graphics.newImage("resources/man.png"), "WalkDown")
@@ -16,6 +19,17 @@ end
 
 function Player:draw()
     self.image:draw(self.x, self.y)
+    
+    -- Debug draw collisions
+    local items, len = collision_world:getItems()
+
+    for _, item in pairs(items) do
+      local rect_x, rect_y, rect_w, rect_h = collision_world:getRect(item)
+      love.graphics.setColor(1.0, 0, 0, 1.0)
+      love.graphics.print(("Pos: %d %d"):format(rect_x, rect_y), rect_x, rect_y - 16)
+      love.graphics.print(("Size: %d %d"):format(rect_w, rect_h), rect_x, rect_y - 32)
+      love.graphics.rectangle("line", rect_x, rect_y, rect_w, rect_h)
+    end
 end
 
 function Player:update(dt)
