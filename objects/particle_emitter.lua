ca66224c8d71f71945bbbe17cf4546997b356064
FHoughton <fdphoughton@protonmail.com>
2024-08-18 17:15:37 +0100

    Fix particle system speedup issue

--- objects/particle_emitter.lua
+++ objects/particle_emitter.lua
@@ -5,48 +5,60 @@ local ParticleEmitter = GameObject:extend()
 local allParticleData = require("resources.particles.colorful")
 
 function ParticleEmitter:new(x, y, xscale, yscale, rotation)
-    ParticleEmitter.super.new(self,x,y)
-	self.xscale = xscale
-	self.yscale = yscale
-	self.rotation = rotation
+    ParticleEmitter.super.new(self, x, y)
+    self.xscale = xscale
+    self.yscale = yscale
+    self.rotation = rotation
+
+    -- Deep copy particle systems for this instance
+    self.particleData = {}
+    for _, particleData in ipairs(allParticleData) do
+        local copy = {
+            system = particleData.system:clone(),
+            blendMode = particleData.blendMode,
+            shader = particleData.shader,
+            x = particleData.x,
+            y = particleData.y,
+            kickStartSteps = particleData.kickStartSteps,
+            kickStartDt = particleData.kickStartDt,
+            emitAtStart = particleData.emitAtStart
+        }
+        table.insert(self.particleData, copy)
+    end
+
     self:reset()
 end
 
 function ParticleEmitter:reset()
-	for _, particleData in ipairs(allParticleData) do
-		-- Note that particle systems are already started when created, so we
-		-- don't need to call particleSystem:start() at any point.
-		local particleSystem = particleData.system
-
-		particleSystem:reset()
-		particleSystem:setPosition(allParticleData.x+particleData.x, allParticleData.y+particleData.y)
-
-		for step = 1, particleData.kickStartSteps do -- kickStartSteps may be 0.
-			particleSystem:update(particleData.kickStartDt)
-		end
-
-		particleSystem:emit(particleData.emitAtStart) -- emitAtStart may be 0.
-	end
+    for _, particleData in ipairs(self.particleData) do
+        local particleSystem = particleData.system
+        particleSystem:reset()
+        particleSystem:setPosition(particleData.x, particleData.y)
+        for step = 1, particleData.kickStartSteps do
+            particleSystem:update(particleData.kickStartDt)
+        end
+        particleSystem:emit(particleData.emitAtStart)
+    end
 end
 
 function ParticleEmitter:update(dt)
-	for _, particleData in ipairs(allParticleData) do
-		particleData.system:update(dt)
-	end
+    for _, particleData in ipairs(self.particleData) do
+        particleData.system:update(dt)
+    end
 end
 
 function ParticleEmitter:draw()
     local w, h = love.graphics.getDimensions()
-	local oldBlendMode = love.graphics.getBlendMode()
+    local oldBlendMode = love.graphics.getBlendMode()
 
-	-- Draw all particle systems in the middle of the screen.
-	for _, particleData in ipairs(allParticleData) do
-		love.graphics.setBlendMode(particleData.blendMode)
-		love.graphics.setShader(particleData.shader) -- The .shader field is always nil in this example, so this line actually does nothing.
-		love.graphics.draw(particleData.system, self.x, self.y, self.rotation, self.xscale, self.yscale)
-	end
+    for _, particleData in ipairs(self.particleData) do
+        love.graphics.setBlendMode(particleData.blendMode)
+        love.graphics.setShader(particleData.shader)
+        love.graphics.draw(particleData.system, self.x, self.y, self.rotation, self.xscale, self.yscale)
+    end
 
-	love.graphics.setBlendMode(oldBlendMode)
+    love.graphics.setBlendMode(oldBlendMode)
 end
+
 
 return ParticleEmitter
